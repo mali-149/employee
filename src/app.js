2b36c7bf292aa7d00eaa0fa0ef85179dcfecf1d9
mali-149 <muhammadalidev149@gmail.com>
2024-08-18 12:00:00 +0500

    whatsap

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const employeeRoutes = require("./routes/employeeRoutes");
+const whatsappRoutes = require("./routes/whatsappRoutes");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/employees", employeeRoutes);
+app.use("/api/whatsapp", whatsappRoutes);
 
 // Test API
 app.get("/", (req, res) => {
